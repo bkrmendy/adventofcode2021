7b2d7c016f23101e29386fcd08675b6aa0188934
Berci Kormendy <korber314@duck.com>
2022-10-09 16:27:00 +0200

    eliminate separate challenge folders

--- challenges/Day01.hs
+++ challenges/Day01.hs
@@ -7,13 +7,13 @@ parse :: String -> [Int]
 parse = map readInt . lines
 
 countIncreasing :: [Int] -> Int
-countIncreasing ns = length $ filter (uncurry (<)) $ zip ns (tail ns) 
+countIncreasing ns = length $ filter (uncurry (<)) $ zip ns (tail ns)
 
 part1 :: [Int] -> Int
 part1 = countIncreasing
 
 part2 :: [Int] -> Int
-part2 = countIncreasing . map sum . threes 
+part2 = countIncreasing . map sum . threes
 
 main :: IO ()
 main = challenge 1 parse part1 part2
